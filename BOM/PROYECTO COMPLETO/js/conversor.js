becdbac9d20072032578f8e17470991b4801f8c0
gramirezgh <guerynextu@gmail.com>
2017-07-23 16:36:24 -0400

    proyecto completo
    proyecto completo

---
+++ BOM/PROYECTO COMPLETO/js/conversor.js
@@ -0,0 +1,43 @@
+function convertir(){
+    var numero1 = parseInt(document.getElementById('numero1').value);
+    var resultado;
+    var opcion = parseInt(document.getElementById('opciones').value);
+    
+    if(validar(numero1)){
+        switch(opcion){
+            case 0:
+                alert("porfavor elija una opcion");
+                break;
+            case 1:
+                resultado = numero1+273.15;
+                break;
+            case 2:
+                resultado = numero1*(1.8)+32;
+                break;
+            case 3:
+                resultado = ((numero1-32)*5)/9;
+                break;
+            case 4:
+                resultado = (((numero1-32)*5)/9)+273.15;
+                break;
+            case 5:
+                resultado = numero1-273.15;
+                break;
+            case 6:
+                resultado = ( ( ( (numero1-273.15)*9 ) )/5 )+32;
+                break;
+        }
+        document.getElementById("resultado").value=resultado;
+    }else{
+        alert("Formato incorrecto de grados");
+        document.getElementsById("opciones").selectedIndex="0";
+    }
+}
+//funcion para validar si ingresa un numero por el usuario
+function validar(num1) {
+    if(isNaN(num1)){
+        return false;
+    }else{
+        return true;
+    }
+}
